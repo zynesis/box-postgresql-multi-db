69119ce1db93b4cc08db7ad2f262133066bb6e51
U-Zyn Chua <chua@uzyn.com>
2014-12-02 21:36:01 +0800

    Alter pg_hba.

--- script.sh
+++ script.sh
@@ -2,14 +2,15 @@
 sudo apt-get update -y
 sudo apt-get install postgresql -y
 
-echo "host all all 0.0.0.0/0 md5" | sudo tee -a /etc/postgresql/9.3/main/pg_hba.conf 
+sudo sed -i 's/postgres\s*peer/postgres trust/g' /etc/postgresql/9.1/main/pg_hba.conf
+echo "host all all 0.0.0.0/0 md5" | sudo tee -a /etc/postgresql/9.1/main/pg_hba.conf 
 sudo service postgresql restart
 
 for i in $(seq 1 5);
 do
 	export database"$i"="werckerdb"$i
 	export user"$i"="postgres"$i
-	sudo -u postgres psql -c "CREATE USER postgres"$i" WITH PASSWORD 'wercker';"
-	sudo -u postgres psql -c "CREATE DATABASE werckerdb"$i";"
-	sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE werckerdb"$i" TO postgres"$i";"
+	psql -Upostgres -c "CREATE USER postgres"$i" WITH PASSWORD 'wercker';"
+	psql -Upostgres -c "CREATE DATABASE werckerdb"$i";"
+	psql -Upostgres -c "GRANT ALL PRIVILEGES ON DATABASE werckerdb"$i" TO postgres"$i";"
 done
