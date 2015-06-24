5c309b2faa3f81badb0a4492ad65474ccff1324a
U-Zyn Chua <chua@uzyn.com>
2015-06-24 16:23:19 +0800

    Make script more durable.

--- script.sh
+++ script.sh
@@ -2,14 +2,14 @@
 sudo apt-get update -y
 sudo apt-get install postgresql -y
 
-sudo sed -i 's/postgres\s*peer/postgres trust/g' /etc/postgresql/9.1/main/pg_hba.conf
-echo "host all all 0.0.0.0/0 md5" | sudo tee -a /etc/postgresql/9.1/main/pg_hba.conf 
-echo "listen_addresses='*'" | sudo tee -a  /etc/postgresql/9.1/main/postgresql.conf
-echo "fsync = off" | sudo tee -a  /etc/postgresql/9.1/main/postgresql.conf
-echo "synchronous_commit = off" | sudo tee -a  /etc/postgresql/9.1/main/postgresql.conf
-echo "full_page_writes = off" | sudo tee -a  /etc/postgresql/9.1/main/postgresql.conf
-echo "checkpoint_segments = 30" | sudo tee -a  /etc/postgresql/9.1/main/postgresql.conf
-echo "checkpoint_timeout = 1h" | sudo tee -a  /etc/postgresql/9.1/main/postgresql.conf
+sudo sed -i 's/postgres\s*peer/postgres trust/g' /etc/postgresql/**/main/pg_hba.conf
+echo "host all all 0.0.0.0/0 md5" | sudo tee -a /etc/postgresql/**/main/pg_hba.conf 
+echo "listen_addresses='*'" | sudo tee -a  /etc/postgresql/**/main/postgresql.conf
+echo "fsync = off" | sudo tee -a  /etc/postgresql/**/main/postgresql.conf
+echo "synchronous_commit = off" | sudo tee -a  /etc/postgresql/**/main/postgresql.conf
+echo "full_page_writes = off" | sudo tee -a  /etc/postgresql/**/main/postgresql.conf
+echo "checkpoint_segments = 30" | sudo tee -a  /etc/postgresql/**/main/postgresql.conf
+echo "checkpoint_timeout = 1h" | sudo tee -a  /etc/postgresql/**/main/postgresql.conf
 sudo service postgresql restart
 
 for i in $(seq 1 5);
